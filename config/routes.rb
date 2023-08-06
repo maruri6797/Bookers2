593cd3722bc2e5a6b2cada7f6360d206b561a66e
maruri6797 <138319041+maruri6797@users.noreply.github.com>
2023-08-06 05:14:04 +0000

    [Start]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :books
   devise_for :users
+  resources :books, only: [:new, :create, :index, :edit, :show, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
   root to: "homes#top"
+  get '/home/about' => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2023_08_04_150328) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
