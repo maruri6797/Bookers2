914751841b79c7e1e53a3462ab84a467356da33f
maruri6797 <138319041+maruri6797@users.noreply.github.com>
2023-08-07 08:15:15 +0000

    [Finish]finish...?

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
     else
+      @user = @book.user
       @books = Book.all
       render :index
     end
